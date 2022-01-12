e9671a721cdd3bfdfee4b2211824026b1dafec2b
FabioSDA61 <fabio@uhubs.co.uk>
2022-01-12 16:22:22 +0100

    test

--- uhubs.js
+++ uhubs.js
@@ -2,6 +2,7 @@
 
 
 const buckets = document.getElementsByClassName("bucket-container");
+console.log(buckets)
 
 for (let i = 0; i < buckets.length; i++) {
     const bucketName = buckets[i].querySelector('.bucket-name')
