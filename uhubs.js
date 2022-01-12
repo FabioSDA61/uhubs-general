7c031f8d7b96efb24a3ec1bbe2e17a98f0436e22
FabioSDA61 <fabio@uhubs.co.uk>
2022-01-12 16:58:19 +0100

    test

--- uhubs.js
+++ uhubs.js
@@ -5,9 +5,15 @@ const buckets = document.getElementsByClassName("populate-w-data");
 console.log(buckets)
 
 for (let i = 0; i < buckets.length; i++) {
-    const bucketName = buckets[i].querySelector('.bucket-name').innerText
-    buckets[i].querySelector('.bucket-name-input').value = bucketName
+    ['bucket-name', 
+    'bucket-description', 
+    'bucket-description', 
+    'bucket-duration', 
+    'bucket-order',
+    'bucket-status'].forEach( function (className) {
+        const bucketName = buckets[i].querySelector(className).innerText;
+        buckets[i].querySelector(className + "-input").value = bucketName;
+    });
     
-    console.log(bucketName)
   }
 
