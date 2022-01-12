e5af6cfe8c2e1d1061d99b86f938dc409a13274b
FabioSDA61 <fabio@uhubs.co.uk>
2022-01-12 16:44:00 +0100

    test

--- uhubs.js
+++ uhubs.js
@@ -5,7 +5,7 @@ const buckets = document.getElementsByClassName("populate-w-data");
 console.log(buckets)
 
 for (let i = 0; i < buckets.length; i++) {
-    const bucketName = buckets[i].querySelector('.bucket-name')
+    const bucketName = buckets[i].querySelector('.bucket-name').innerText
     buckets[i].querySelector('.bucket-name-input').value = bucketName
     
     console.log(bucketName)
