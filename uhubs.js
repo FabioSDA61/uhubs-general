305777129a67c28782c8841e08dec21e374d36be
FabioSDA61 <fabio@uhubs.co.uk>
2022-01-12 16:20:54 +0100

    test

--- uhubs.js
+++ uhubs.js
@@ -1,4 +1,10 @@
-//test
 
-console.log('test y')
+
+
+const buckets = document.getElementsByClassName("bucket-container");
+
+for (let i = 0; i < buckets.length; i++) {
+    const bucketName = buckets[i].querySelector('.bucket-name')
+    console.log(bucketName)
+  }
 
