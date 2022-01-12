3cc9bd94d6410555f788a01f9d1c9fca792a3a2e
FabioSDA61 <fabio@uhubs.co.uk>
2022-01-12 17:02:05 +0100

    test

--- uhubs.js
+++ uhubs.js
@@ -11,6 +11,7 @@ for (let i = 0; i < buckets.length; i++) {
     'bucket-duration', 
     'bucket-order',
     'bucket-status'].forEach( function (className) {
+        console.log(className)
         const bucketName = buckets[i].querySelector(className).innerText;
         buckets[i].querySelector(className + "-input").value = bucketName;
     });
