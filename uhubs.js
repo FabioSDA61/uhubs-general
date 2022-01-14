24e00b87237146c6828a12fce51621a5b875c00f
FabioSDA61 <fabio@uhubs.co.uk>
2022-01-14 16:39:22 +0100

    Test

--- uhubs.js
+++ uhubs.js
@@ -30,3 +30,40 @@ for (let i = 0; i < buckets.length; i++) {
         });
       }
   }
+
+  function handleUpdate () {
+    
+      for (let i = 0; i < buckets.length; i++) {
+
+        buckets[i].querySelector("update-bucket-button").onclick = function(){
+
+        ['.bucket-name',
+        '.bucket-description', 
+        '.bucket-duration', 
+        '.bucket-order',
+        '.bucket-status'].forEach( function (className) {
+          let data = buckets[i].querySelector(className + "-input").value
+          buckets[i].querySelector(className).innerText = data
+        });
+      };
+    };
+
+    for (let a = 1; a < 6; a++) {
+      buckets[i].querySelector("update-task-button" + [a].toString()).onclick = function(){
+        ['.task-name',
+          '.task-description', 
+          '.task-duration',
+          '.task-link',
+          '.task-type'].forEach( function (classNameTask) {
+              let classNameTaskIteration = classNameTask + "-" + [a].toString()
+              let data = buckets[i].querySelector(classNameTaskIteration + "-input").value;
+              if (classNameTask !='.task-link') {
+                buckets[i].querySelector(classNameTaskIteration).innerText = data;
+              } else {
+                buckets[i].querySelector(classNameTaskIteration).href = data;
+              }
+          });
+        }
+      }
+  };
+
