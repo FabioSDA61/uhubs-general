1d7984fe87add3e74871a64e77f3b0e6ef3e9662
FabioSDA61 <fabio@uhubs.co.uk>
2022-01-25 02:54:59 +0100

    Update uhubs.js

--- uhubs.js
+++ uhubs.js
@@ -86,27 +86,36 @@ handleUpdate();
 
 } else {
 
-  console.log('not manager')
+  let confettiButtons = document.getElementsByClassName("confetti-button");
+
+  for (let a = 0; a < confettiButtons.length; a++) {
+    console.log('confetti attached')
+    
+    confettiButtons[a].on('click', function(){  
+      function random(max){
+          return Math.random() * (max - 0) + 0;
+      }
   
-  $('.confetti-button').on('click', function(){  
-    function random(max){
-        return Math.random() * (max - 0) + 0;
+      console.log('click')
+    
+      var c = document.createDocumentFragment();
+      for (var i=0; i<100; i++) {
+        var styles = 'transform: translate3d(' + (random(500) - 250) + 'px, ' + (random(200) - 150) + 'px, 0) rotate(' + random(360) + 'deg);\
+                      background: hsla('+random(360)+',100%,50%,1);\
+                      animation: bang 700ms ease-out forwards;\
+                      opacity: 0';
+          
+        var e = document.createElement("i");
+        e.style.cssText = styles.toString();
+        c.appendChild(e);
+    }
+    // document.body.appendChild(c);
+      $(this).append(c);
+    })
+
     }
   
-    var c = document.createDocumentFragment();
-    for (var i=0; i<100; i++) {
-      var styles = 'transform: translate3d(' + (random(500) - 250) + 'px, ' + (random(200) - 150) + 'px, 0) rotate(' + random(360) + 'deg);\
-                    background: hsla('+random(360)+',100%,50%,1);\
-                    animation: bang 700ms ease-out forwards;\
-                    opacity: 0';
-        
-      var e = document.createElement("i");
-      e.style.cssText = styles.toString();
-      c.appendChild(e);
-  }
-  // document.body.appendChild(c);
-    $(this).append(c);
-  })
+  
 };
 
 
