a43bf8ab6bea518d9408959bb1bddb1fb387f6a0
FabioSDA61 <fabio@uhubs.co.uk>
2022-02-17 13:17:57 +0100

    try

--- uhubs.js
+++ uhubs.js
@@ -26,6 +26,392 @@ setTimeout(() => {
 
 
 
+// global info 
+let managerBuckets = document.querySelectorAll('.edit-view-bucket');
+let allMembers = [];
+ let hiddenEmails = document.querySelectorAll('.team-member-email-hidden')
+ for (var i = 0; i < hiddenEmails.length; i++) {
+     let email = hiddenEmails[i].innerText;
+     allMembers.push(email);
+ }
+
+
+
+ function assignmentCreateBucket() {
+   // bucket state
+   const bucket = document.querySelector('.bucket-create-form')
+   let assignedList = [];
+   let notAssignedList = [];
+   for (var i = 0; i < hiddenEmails.length; i++) {
+     let email = hiddenEmails[i].innerText;
+     assignedList.push(email);
+ }
+
+   let allMembersContainer = bucket.querySelector('.all-members-container');
+   let hiddenInputAssigned = bucket.querySelector('.assigned-input');
+   let hiddenInputUnassigned = bucket.querySelector('.unassigned-input');
+
+   hiddenInputAssigned.value = assignedList.join(', ');
+   hiddenInputUnassigned.value = notAssignedList.join(', ');
+
+   
+
+
+
+
+   //toggle assign-all button of and delete-all on if all members are already invited
+   function toggleAll() {
+       if (assignedList.length == allMembers.length) {
+           removeAll();
+       } else {
+           assignAll();
+       }
+   }
+
+   function createToggleAllButton(){
+
+
+       let toggleAllButton = bucket.querySelector('.toggle-all-button');
+           if (toggleAllButton) { toggleAllButton.remove()};
+
+       let childNode = document.createElement('input');
+       childNode.type = "checkbox"
+       childNode.className = 'toggle-all-button'
+       childNode.checked =  assignedList.length == allMembers.length ? true : false;
+       childNode.onclick = function (){ 
+           toggleAll();
+       }
+       bucket.querySelector('.toggle-all-container').appendChild(childNode);
+   }
+   createToggleAllButton();
+
+
+   function assignAll() {
+
+           assignedList = [];
+           assignedList.push(...allMembers)
+           notAssignedList = [];            
+
+           allMembersContainer.innerHTML = '';
+           hiddenInputAssigned.value = '';
+           hiddenInputUnassigned.value = '';
+           hiddenInputAssigned.value = assignedList.join(', ')
+
+           for (let d = 0; d < assignedList.length; d++) {
+               createMemberToggler(assignedList[d]);
+           }
+           createToggleAllButton();
+   }
+
+   function removeAll() {
+
+     notAssignedList = [];
+     notAssignedList.push(...allMembers)
+     assignedList= [];
+
+       allMembersContainer.innerHTML = '';
+       hiddenInputAssigned.value = '';
+       hiddenInputUnassigned.value = '';
+       hiddenInputUnassigned.value = notAssignedList;
+
+       for (let d = 0; d < notAssignedList.length; d++) {
+           createMemberToggler(notAssignedList[d]);
+           }
+
+       for (let d = 0; d < assignedList.length; d++) {
+           createMemberToggler(assignedList[d]);
+       }
+
+       createToggleAllButton();
+   }
+
+
+
+   function updateItem (element, id) {
+
+       let isAssigned = assignedList.includes(element.id);
+
+       if (isAssigned) {
+           notAssignedList.push(element.id)
+           const index = assignedList.indexOf(element.id);
+           if (index > -1) {
+               assignedList.splice(index, 1); 
+           }
+       } else {
+           assignedList.push(element.id)
+           const index = notAssignedList.indexOf(element.id);
+           if (index > -1) {
+               notAssignedList.splice(index, 1); 
+           }
+       }
+
+       hiddenInputAssigned.value = '';
+       hiddenInputAssigned.value = assignedList.join(', ');
+       hiddenInputUnassigned.value = '';
+       hiddenInputUnassigned.value = notAssignedList.join(', ');
+
+   
+       createToggleAllButton();                
+   }
+
+
+   function createMemberToggler(email){
+       let name = email.split('@')[0];
+
+       let containerChildNode = document.createElement('div');
+       containerChildNode.className = name + '-container member-toggler-container';
+
+       let checkboxChildNode = document.createElement('input');
+       checkboxChildNode.type = "checkbox"
+       checkboxChildNode.className = name
+       checkboxChildNode.id = email
+       checkboxChildNode.checked =  assignedList.includes(email)
+       checkboxChildNode.onclick = function (){ 
+           updateItem(this);
+       }
+       containerChildNode.appendChild(checkboxChildNode);
+
+       let childNodeText = document.createElement('p');   
+       childNodeText.className = 'paragraph small no-margin _12px opaque50';                    
+       childNodeText.innerText = email
+       childNodeText.style.paddingLeft = '10px'
+       containerChildNode.appendChild(childNodeText);
+
+       allMembersContainer.appendChild(containerChildNode);
+   }
+
+   //initialPopulation
+
+
+   for (let d = 0; d < assignedList.length; d++) {
+       createMemberToggler(assignedList[d]);
+   }
+
+   for (let d = 0; d < notAssignedList.length; d++) {
+       createMemberToggler(notAssignedList[d]);
+   }
+
+}
+assignmentCreateBucket();
+
+
+//enable team member assignment on every bucket    
+for (var n = 0; n < managerBuckets.length; n++) {
+
+ function sortAssignmentsPerBucket() {
+
+        // bucket state
+        const bucket = managerBuckets[n]
+        let assignedList = [];
+        if (bucket.querySelector('.assigned-team-members')) {
+         assignedList = bucket.querySelector('.assigned-team-members').innerText ? bucket.querySelector('.assigned-team-members').innerText.split(', ') : [];
+       } 
+        let notAssignedList = [];
+        let assignedMembersContainer = bucket.querySelector('.assigned-members-container');
+        let allMembersContainer = bucket.querySelector('.all-members-container');
+        let hiddenInputAssigned = bucket.querySelector('.assigned-input');
+        let hiddenInputUnassigned = bucket.querySelector('.unassigned-input');
+        //add not assignement members to noAssignedList
+        for (var a = 0; a < allMembers.length; a++) {
+            if (!assignedList.includes(allMembers[a])) {
+                notAssignedList.push(allMembers[a])
+            }
+        }
+
+        //toggle assign-all button of and delete-all on if all members are already invited
+        function toggleAll() {
+            if (assignedList.length == allMembers.length) {
+                removeAll();
+            } else {
+                assignAll();
+            }
+        }
+
+        function createToggleAllButton(){
+
+            let toggleAllButton = bucket.querySelector('.toggle-all-button');
+                if (toggleAllButton) { toggleAllButton.remove()};
+
+            let childNode = document.createElement('input');
+            childNode.type = "checkbox"
+            childNode.className = 'toggle-all-button'
+            childNode.checked =  assignedList.length == allMembers.length ? true : false;
+            childNode.onclick = function (){ 
+                toggleAll();
+            }
+            bucket.querySelector('.toggle-all-container').appendChild(childNode);
+        }
+        createToggleAllButton();
+
+
+        function assignAll() {
+
+
+               assignedList = [];
+                assignedList.push(...allMembers)
+                notAssignedList = [];
+
+                assignedMembersContainer.innerHTML = '';
+                allMembersContainer.innerHTML = '';
+
+                hiddenInputAssigned.value = '';
+                hiddenInputAssigned.value = assignedList.join(', ')
+                hiddenInputUnassigned.value = '';
+                hiddenInputUnassigned.value = notAssignedList.join(', ')
+
+                for (let d = 0; d < assignedList.length; d++) {
+                    createMemberToggler(assignedList[d]);
+                }
+
+                /*
+                for (var b = 0; b < assignedList.length; b++) {
+                    let childNode = document.createElement("p")
+                    childNode.className = 'paragraph small no-margin _12px opaque50';
+                   childNode.style.marginBottom = '5px';
+                    childNode.innerText = assignedList[b]
+                    assignedMembersContainer.appendChild(childNode);
+                }
+                */
+                let childNode = document.createElement("p")
+                childNode.innerHTML = "All Team Members"
+                childNode.className = 'paragraph small no-margin _12px opaque50';
+                assignedMembersContainer.appendChild(childNode);
+
+                createToggleAllButton();
+        }
+
+        function removeAll() {
+
+           notAssignedList = [];
+            notAssignedList.push(...allMembers)
+            assignedList= [];
+
+
+            bucket.querySelector('.assigned-members-container');
+
+            assignedMembersContainer.innerHTML = '';
+            allMembersContainer.innerHTML = '';
+   
+            hiddenInputAssigned.value = '';
+            hiddenInputAssigned.value = assignedList.join(', ')
+            hiddenInputUnassigned.value = '';
+            hiddenInputUnassigned.value = notAssignedList.join(', ')
+
+            for (let d = 0; d < notAssignedList.length; d++) {
+                createMemberToggler(notAssignedList[d]);
+                }
+
+            for (let d = 0; d < assignedList.length; d++) {
+                createMemberToggler(assignedList[d]);
+            }
+            /*
+            for (var b = 0; b < assignedList.length; b++) {
+                let childNode = document.createElement("p")
+                childNode.className = 'paragraph small no-margin _12px opaque50';
+                childNode.style.marginBottom = '5px';
+                childNode.innerText = assignedList[b]
+                assignedMembersContainer.appendChild(childNode);
+            }
+            */
+            let childNode = document.createElement("p")
+            childNode.className = 'paragraph small no-margin _12px opaque50';
+            childNode.style.opacity = '0.25'
+            childNode.innerHTML = "none"
+            assignedMembersContainer.appendChild(childNode);
+
+            createToggleAllButton();
+        }
+
+
+
+        function updateItem (element, id) {
+            let isAssigned = assignedList.includes(element.id);
+ 
+            if (isAssigned) {
+                notAssignedList.push(element.id)
+                const index = assignedList.indexOf(element.id);
+                if (index > -1) {
+                    assignedList.splice(index, 1); 
+                }
+            } else {
+                assignedList.push(element.id)
+                const index = notAssignedList.indexOf(element.id);
+                if (index > -1) {
+                    notAssignedList.splice(index, 1); 
+                }
+            }
+
+            assignedMembersContainer.innerHTML = '';
+
+            hiddenInputAssigned.value = '';
+            hiddenInputAssigned.value = assignedList.join(', ')
+            hiddenInputUnassigned.value = '';
+            hiddenInputUnassigned.value = notAssignedList.join(', ')
+
+
+            for (var i = 0; i < assignedList.length; i++) {
+                let childNode = document.createElement("p")
+                childNode.className = 'paragraph small no-margin _12px opaque50';
+                childNode.style.marginBottom = '5px';
+                childNode.innerText = assignedList[i]
+                assignedMembersContainer.appendChild(childNode);
+            }
+   
+            createToggleAllButton();                
+        }
+
+
+        function createMemberToggler(email){
+            let name = email.split('@')[0];
+
+            let containerChildNode = document.createElement('div');
+            containerChildNode.className = name + '-container member-toggler-container';
+
+            let checkboxChildNode = document.createElement('input');
+            checkboxChildNode.type = "checkbox"
+            checkboxChildNode.className = name
+            checkboxChildNode.id = email
+            checkboxChildNode.checked =  assignedList.includes(email)
+            checkboxChildNode.onclick = function (){ 
+                updateItem(this);
+            }
+            containerChildNode.appendChild(checkboxChildNode);
+
+            let childNodeText = document.createElement('p');   
+            childNodeText.className = 'paragraph small no-margin _12px opaque50';                    
+            childNodeText.innerText = email
+            childNodeText.style.paddingLeft = '10px'
+            containerChildNode.appendChild(childNodeText);
+
+            allMembersContainer.appendChild(containerChildNode);
+        }
+
+        //initialPopulation
+
+        hiddenInputAssigned.value = assignedList.join(', ');
+        hiddenInputUnassigned.value = notAssignedList.join(', ');
+
+        for (let d = 0; d < assignedList.length; d++) {
+            createMemberToggler(assignedList[d]);
+        }
+
+        for (let d = 0; d < notAssignedList.length; d++) {
+            createMemberToggler(notAssignedList[d]);
+        }
+
+        for (var b = 0; b < assignedList.length; b++) {
+ 
+            let childNode = document.createElement("p")
+            childNode.className = 'paragraph small no-margin _12px opaque50';
+             childNode.style.marginBottom = '5px';
+            childNode.innerText = assignedList[b]
+            assignedMembersContainer.appendChild(childNode);
+        }   
+ }
+ sortAssignmentsPerBucket();
+}
+
+
 //------------------------------------------------ OLD START
 
 /*
