0a08433a839f6d149dc2d079fd2e55b5ca6ae50e
FabioSDA61 <fabio@uhubs.co.uk>
2022-02-22 11:03:32 +0100

    try

--- uhubs.js
+++ uhubs.js
@@ -58,7 +58,7 @@ const buckets = document.getElementsByClassName("bucket");
           }
       }
     }
-    //populateForms();
+    populateForms();
     
     
     // Handle update of buckets and tasks i.e. update tasks and bucket immediately after form submission
