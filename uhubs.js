4233a6a0781fab7bd73ed2c5684bb8c11445e15f
FabioSDA61 <fabio@uhubs.co.uk>
2022-03-24 16:23:48 +0100

    try

--- uhubs.js
+++ uhubs.js
@@ -150,8 +150,7 @@ const buckets = document.getElementsByClassName("bucket");
               
               ['.task-name',
                 '.task-description', 
-                '.task-duration',
-                '.task-link'
+                '.task-duration'
                 ].forEach( function (classNameTask) {
                     let classNameTaskIteration = classNameTask + "-" + [a].toString()
                     console.log(classNameTaskIteration)
